689b08104cc898a14bafd8c42a28a381d7cad0d7
leo-liuzy <zeyuliu2@cs.washington.edu>
2019-08-29 21:28:11 -0700

    add toy model

---
+++ scripts/generative_story/basic.py
@@ -0,0 +1,67 @@
+from typing import *
+import pickle
+from tqdm import tqdm
+import os
+import numpy as np
+from scipy.special import softmax
+
+np.random.seed(6)
+
+alpha = 1
+d_dim = 20
+s_dim = d_dim  # assumption of basic model
+num_doc = 20000
+vocab_size = 3001
+result_dir = "/home/lzy/proj/neural_persona/examples/toy/basic"
+
+num_word_func = lambda: np.random.randint(100, 200)
+num_entity_func = lambda: np.random.randint(1, 20)
+entity_func = lambda x:  np.random.multivariate_normal(x, np.eye(x.shape[0]))
+
+pk_output: List[Dict[str, np.ndarray]] = []
+
+# global parameters
+beta = np.random.dirichlet(np.ones(vocab_size) * alpha, d_dim)
+b = softmax(np.ones(vocab_size))
+
+for i in tqdm(range(num_doc)):
+    # sample a document representation
+    d = np.random.standard_normal(d_dim)
+    theta = softmax(d)
+
+    # sample number of entities in the document
+    E_i = num_entity_func()
+    # sample number of words in the document
+    C_i = num_word_func()
+    # calculate number of word per entity in the document
+    C_ij = C_i // E_i
+
+    # sample s_i from N(d, 1)
+    s_i = np.random.multivariate_normal(d, np.eye(d.shape[0]), size=E_i)
+    e_i = softmax(s_i, axis=-1)
+    p_s_i = softmax(e_i @ beta + b, axis=-1)
+    k = C_ij // 2
+
+    # reconstruct entity BoW vector
+    entities_vector = np.zeros_like(p_s_i)
+    indices = np.argsort(p_s_i, axis=-1)[:, -k:]
+    for idx, indice in enumerate(indices):
+        entities_vector[idx, indice] = np.rint(softmax(p_s_i[idx, indice], axis=-1) * C_ij)
+
+    # reconstruct entity BoW vector
+    p_x = softmax(d @ beta + b, axis=-1)
+    doc_vector = np.zeros_like(p_x)
+    indice = np.argsort(p_x)[-C_i // 2:]
+    doc_vector[indice] = np.rint(softmax(p_x[indice], axis=-1) * C_i)
+    pk_output.append({"doc": doc_vector, "entities": entities_vector})
+
+# create folder if not exists
+if not os.path.exists(result_dir):
+    os.makedirs(result_dir)
+
+np.random.shuffle(pk_output)
+train = pk_output[:num_doc // 10 * 8]
+dev = pk_output[num_doc // 10 * 8:]
+
+pickle.dump(train, open(f"{result_dir}/train.pk", "wb"))
+pickle.dump(dev, open(f"{result_dir}/dev.pk", "wb"))
